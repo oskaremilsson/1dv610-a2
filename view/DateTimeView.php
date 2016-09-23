dc531da15b2d2e43eda6b104e326af8453b53ca3
Oskar Emilsson <oskaremilsson@gmail.com>
2016-09-23 12:43:28 +0200

    Fixing bug with dates suffix

--- view/DateTimeView.php
+++ view/DateTimeView.php
@@ -7,7 +7,7 @@ class DateTimeView {
 		//Saturday, the 17th of September 2016, The time is
 		$time = getdate();
 
-		$timeString = $time['weekday'] . ", the " . $time['mday'] . "th of ". $time['month'] . " " . $time['year'] .
+		$timeString = $time['weekday'] . ", the " . date("dS") . " of " . $time['month'] . " " . $time['year'] .
 			", The time is " . $time['hours'] . ":" . $time['minutes'] . ":" . $time['seconds'];
 
 		return '<p>' . $timeString . '</p>';
